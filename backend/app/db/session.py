1fc2f35e58432d895b5ea64dfcf6692345b37698
Camzero <u5500360@unimilitar.edu.co>
2022-08-02 17:40:20 +0800

    Fix db session connection

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core import config
-engine = create_engine(config.SQLALCHEMY_DATABASE_URI )
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI , echo=True, echo_pool=True,pool_pre_ping=True,pool_recycle=30 )
 
 SessionLocal = sessionmaker( autocommit=False,autoflush=False, bind=engine)
 Base = declarative_base()
@@ -13,7 +13,7 @@ def get_db():
 	db = SessionLocal()
 	try:
 		yield db
-	except:
+	finally:
 		db.close()
 
 get_db()
